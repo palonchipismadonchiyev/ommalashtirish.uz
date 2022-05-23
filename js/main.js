2986a9b05f313fcddb4a82e9166dd617da9ca500
palonchipismadonchiyev <palonchipismadonchiyev7@google.com>
2022-05-23 16:59:56 +0500

    enasini emsin zerikdim

--- js/main.js
+++ js/main.js
@@ -1,24 +1,38 @@
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     loop: true,
-    margin: 10,
+    margin: 30,
     nav: false,
     responsive: {
       0: {
         items: 1,
       },
-      400: {
+      500: {
         items: 2,
       },
-      800: {
+      750: {
         items: 3,
       },
-      1200: {
+      1000: {
         items: 4,
       },
-      1600: {
-        items: 5,
-      },
     },
+  });
+  // scroll menu effect
+  $(".menu-item").click(function (e) {
+    e.preventDefault();
+    var aid = $(this).attr("href");
+    $("html,body").animate({ scrollTop: $(aid).offset().top }, "slow");
+  });
+  // open sidebar menu
+  $("#openMenu").click(function activMenu(e) {
+    $(".right-sidebar").addClass("activMenu");
+  });
+  $("#closeMenu").click(function activMenu(e) {
+    $(".right-sidebar").removeClass("activMenu");
+  });
+
+  $(".menu-item").click(function activMenu(e) {
+    $(".right-sidebar").removeClass("activMenu");
   });
 });
